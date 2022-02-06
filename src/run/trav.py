4e7ee53c496610b0585b0654a24b408374ec9026
Cho0joy <96289341+Cho0joy@users.noreply.github.com>
2022-02-06 15:47:05 +1000

    Update trav.py

--- src/run/trav.py
+++ src/run/trav.py
@@ -8,8 +8,6 @@ from template_finder import TemplateFinder
 from town.town_manager import TownManager
 from ui import UiManager
 from utils.misc import wait
-
-
 class Trav:
     def __init__(
         self,
@@ -27,7 +25,6 @@ class Trav:
         self._ui_manager = ui_manager
         self._char = char
         self._pickit = pickit
-
     def approach(self, start_loc: Location) -> Union[bool, Location]:
         # Go to Travincal via waypoint
         Logger.info("Run Trav")
@@ -37,14 +34,13 @@ class Trav:
         if self._ui_manager.use_wp(3, 7):
             return Location.A3_TRAV_START
         return False
-
     def battle(self, do_pre_buff: bool) -> Union[bool, tuple[Location, bool]]:
         # Kill Council
         if not self._template_finder.search_and_wait(["TRAV_0", "TRAV_1", "TRAV_20"], threshold=0.65, time_out=20).valid:
             return False
         if do_pre_buff:
             self._char.pre_buff()
-        if self._char.capabilities.can_teleport_natively:
+        if self._char.can_teleport():
             self._pather.traverse_nodes_fixed("trav_safe_dist", self._char)
         else:
             if not self._pather.traverse_nodes((Location.A3_TRAV_START, Location.A3_TRAV_CENTER_STAIRS), self._char, force_move=True):
@@ -52,11 +48,23 @@ class Trav:
         self._char.kill_council()
         picked_up_items = self._pickit.pick_up_items(self._char, is_at_trav=True)
         wait(0.2, 0.3)
+
         # If we can teleport we want to move back inside and also check loot there
-        if self._char.capabilities.can_teleport_natively or self._char.capabilities.can_teleport_with_charges:
-            if not self._pather.traverse_nodes([229], self._char, time_out=2.5, use_tp_charge=True):
-                self._pather.traverse_nodes([228, 229], self._char, time_out=2.5, use_tp_charge=True)
+        picked_up_items = False
+        if self._char.can_teleport():
+            picked_up_items = self._pickit.pick_up_items(self._char, is_at_trav=True)
+            wait(0.2, 0.3)
+            if not self._pather.traverse_nodes([229], self._char, time_out=2.5):
+                self._pather.traverse_nodes([228, 229], self._char, time_out=2.5)
             picked_up_items |= self._pickit.pick_up_items(self._char, is_at_trav=True)
+        else: # Else we need to make sure we loot both inside and outside the council room
+            self._pather.traverse_nodes([228, 226], self._char, time_out=2, force_move=True)
+            picked_up_items |= self._pickit.pick_up_items(self._char, is_at_trav=True)
+            wait(0.2, 0.3)
+            self._pather.traverse_nodes([226, 228, 229], self._char, time_out=2, force_move=True)
+            picked_up_items |= self._pickit.pick_up_items(self._char, is_at_trav=True)
+            wait(0.2, 0.3)
+
         # Make sure we go back to the center to not hide the tp
         self._pather.traverse_nodes([230], self._char, time_out=2.5)
         return (Location.A3_TRAV_CENTER_STAIRS, picked_up_items)
